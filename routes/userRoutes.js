f2c814bf448bb358850cf4662371bb7016cf2951
aliou9sow6 <aliou9sow6@gmail.com>
2026-04-30 10:26:25 +0000

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const userController = require('../controllers/userController');
 
 router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
+router.get('/:id', userController.getUserById); 
+router.put('/:id', userController.updateUser); // PUT: http://localhost:3000/api/users/12345
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
